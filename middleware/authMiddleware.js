1c9da6b27bd4733f8c438ed1b56359ed539cd9a6
Dawid <dawid.jm@interia.pl>
2021-02-23 17:21:08 +0100

    deploy project withsignup & login

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,48 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const checkUser = (req, res, next) => {
+    const token = req.cookies.authenticatedUser;
+
+    if (token) {
+        jwt.verify(token, 'Vt2jne6sk5vSqRXp', async (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.locals.user = null;
+                next();
+            } else {
+                console.log(decodedToken);
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        });
+    } else {
+        res.locals.user = null;
+        next();
+    }
+};
+
+// logged in is needed to watch special pages
+const requireAuth = (req, res, next) => {
+    const token = req.cookies.authenticatedUser;
+
+    if (token) {
+        jwt.verify(token, 'Vt2jne6sk5vSqRXp', async (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.redirect('/home');
+            } else {
+                console.log(decodedToken);
+                next();
+            }
+        });
+    } else {
+        res.redirect('/home');
+    }
+};
+
+module.exports = {
+    checkUser,
+    requireAuth
+};
---
+++ models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const { isStrongPassword } = require('validator');
+const bcrypt = require('bcrypt');
+
+// creating schema
+const userSchema = new mongoose.Schema({
+    login: {
+        type: String,
+        required: [true, 'Plese enter your login'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter a password'],
+        validate: [isStrongPassword, 'Please enter strong password']
+    }
+});
+
+// hash values
+userSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+// static login user method
+userSchema.statics.login = async function(login, password) {
+    if (login.length > 0) {
+        const user = await this.findOne({ login: login });
+        if (user) {
+            const auth = await bcrypt.compare(password, user.password);
+            if (auth) {
+                return user;
+            }
+            throw Error('incorrect password');
+        }
+        throw Error('incorrect login');
+    }
+};
+
+// define model
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
